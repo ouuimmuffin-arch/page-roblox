276484e6be4b4ceba419a55535b4fb0719ae78bb
rennify <shinraya8@email.com>
2025-09-04 18:12:01 +0300

    Initial commit

---
+++ pages/communities/[key].js
@@ -0,0 +1,27 @@
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../../lib/firebase';
+
+export async function getServerSideProps({ params, res }) {
+  const key = params.key;
+
+  const q = query(collection(db, 'urls'), where('key', '==', key));
+  const querySnapshot = await getDocs(q);
+
+  if (querySnapshot.empty) {
+    res.statusCode = 404;
+    return { props: {} };
+  }
+
+  const doc = querySnapshot.docs[0];
+  const urlData = doc.data();
+
+  // Redirect to the original URL
+  res.writeHead(302, { Location: urlData.originalUrl });
+  res.end();
+
+  return { props: {} };
+}
+
+export default function RedirectPage() {
+  return null;
+}
